f035ee356940ced17b214c9c3c6d248191385e4c
GnelShahnazaryan2 <gnel.s@tumo.org>
2024-07-06 17:22:15 +0400

    Data from API set

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const path = require('path');
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 
 const isMac = process.platform === 'darwin';
 
-function createMainWindow(){
-const mainWindow = new BrowserWindow({
-title: "Weather App",
-width: 1260,
-height: 980
-});
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: "Weather App",
+        width: 1260,
+        height: 980,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
 
-mainWindow.loadFile(path.join(__dirname, './app/index.html'));
+    mainWindow.loadFile(path.join(__dirname, './app/index.html'));
 }
 
-app.whenReady().then(()=>{
-createMainWindow();
+app.whenReady().then(() => {
+    createMainWindow();
 
-app.on('activate', ()=>{
-if(BrowserWindow.getAllWindows().length === 0) {
-createMainWindow();
-}
-})
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow();
+        }
+    })
 })
 
 app.on('window-all-closed', () => {
-if(!isMac){
-app.quit();
-}
+    if (!isMac) {
+        app.quit();
+    }
 })
